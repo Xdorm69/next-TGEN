114fd671c797d63b673762f619969a7344887051
amitoj <amitoj.mehta@gmail.com>
2026-01-27 19:40:11 +0530

    test/id api issue

--- app/api/test/[id]/route.ts
+++ app/api/test/[id]/route.ts
@@ -7,43 +7,50 @@ export async function GET(
   req: NextRequest,
   { params }: { params: Promise<{ id: string }> },
 ) {
-  await connectDB();
-  const p = await params;
-  const id = p.id;
-  const test = await Test.findById(id, {
-    name: 1,
-    subject: 1,
-    description: 1,
-    tags: 1,
-    "questions.difficulty": 1,
-  })
-    .populate("subject", "name color")
-    .lean();
+  try {
+    await connectDB();
+    const p = await params;
+    const id = p.id;
+    const test = await Test.findById(id, {
+      name: 1,
+      subject: 1,
+      description: 1,
+      tags: 1,
+      "questions.difficulty": 1,
+    })
+      .populate("subject", "name color")
+      .lean();
 
-  if (!test) {
-    return NextResponse.json({ message: "Test not found" }, { status: 404 });
-  }
-
-  const difficultyCount = {
-    easy: 0,
-    medium: 0,
-    hard: 0,
-  };
-
-  for (const q of test.questions) {
-    if (q.difficulty in difficultyCount) {
-      difficultyCount[q.difficulty as keyof typeof difficultyCount]++;
+    if (!test) {
+      return NextResponse.json({ message: "Test not found" }, { status: 404 });
     }
+
+    const difficultyCount = {
+      easy: 0,
+      medium: 0,
+      hard: 0,
+    };
+
+    for (const q of test.questions) {
+      if (q.difficulty in difficultyCount) {
+        difficultyCount[q.difficulty as keyof typeof difficultyCount]++;
+      }
+    }
+
+    return NextResponse.json({
+      _id: test._id,
+      name: test.name,
+      subject: test.subject.name,
+      subjectColor: test.subject.color,
+      description: test.description,
+      totalQuestions: test.questions.length,
+      difficulty: difficultyCount,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
   }
-
-
-  return NextResponse.json({
-    _id: test._id,
-    name: test.name,
-    subject: test.subject.name,
-    subjectColor: test.subject.color,
-    description: test.description,
-    totalQuestions: test.questions.length,
-    difficulty: difficultyCount,
-  });
 }

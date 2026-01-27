63abd4b0aa69b4f17289a81c29630ea615e05094
amitoj <amitoj.mehta@gmail.com>
2026-01-27 15:41:08 +0530

    first deploy issues fix

--- app/api/admin/subject/route.ts
+++ app/api/admin/subject/route.ts
@@ -2,13 +2,13 @@ import { subjectSchema, SubjectSchema } from "@/lib/validator/subject";
 import { connectDB } from "@/MongoDB/db";
 import { Subject } from "@/MongoDB/models/subject.model";
 import { getAuthUser } from "@/utils/authUtil";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
   try {
     const user = await getAuthUser();
     if (!user || user.role !== "admin") {
-      return Response.json({ error: "Unauthorized" }, { status: 401 });
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     await connectDB();
 
@@ -16,14 +16,14 @@ export async function POST(request: NextRequest) {
     const parsedSubjectData = subjectSchema.safeParse(subjectData);
 
     if (!parsedSubjectData.success) {
-      return Response.json({ error: "Invalid subject data" }, { status: 400 });
+      return NextResponse.json({ error: "Invalid subject data" }, { status: 400 });
     }
 
     const subject = await Subject.create(parsedSubjectData.data);
 
-    return Response.json(subject);
+    return NextResponse.json(subject);
   } catch (error) {
     console.error(error);
-    return Response.json({ error: "Internal server error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
   }
 }

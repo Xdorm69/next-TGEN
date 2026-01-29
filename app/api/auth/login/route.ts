33c4eec97798d5562507d9e1ecfe36f8ab6fae09
amitoj <amitoj.mehta@gmail.com>
2026-01-29 14:18:51 +0530

    cookie for prod issue fix

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -52,7 +52,7 @@ export async function POST(req: Request) {
     response.cookies.set("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 60 * 60 * 24 * 7, // 7 days
       path: "/",
     });
